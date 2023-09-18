0d51aa626b85db0b83b00d0459617fb3401ba8bd
Cristiancm49 <ccortesm2000@gmail.com>
2023-09-17 19:22:32 -0500

    Usado variables y tipos de variables con go

--- src/main.go
+++ src/main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+
+	const pi float64 = 3.14
+	const pi2 = 3.1514
+	
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	// Declaracion de variables
+
+	base := 12
+	var altura int =14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	var a int
+	var b string
+	var c float32
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area cuadrado
+
+	const baseCuadrado int = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+
+	fmt.Println("area cuadrado: ", areaCuadrado)
 }
